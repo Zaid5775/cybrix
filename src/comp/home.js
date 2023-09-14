78e6aa59da2c6c76d8adb037a4f3b109b6c997b8
zaid <szaid5775@gmail.com>
2023-09-15 00:40:11 +0530

    hi

---
+++ src/comp/home.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import logo from './cybrix3.png';
+import './home.css';
+
+const Home = () => {
+    const [url, setUrl] = useState('');
+
+    const handleInputChange = (e) => {
+        setUrl(e.target.value);
+    }
+
+    const handleSubmit = () => {
+        console.log("Your URL is: ", url)
+    }
+
+    return (
+        <div className='main'>
+            <div className='main1'>
+            <div className='img'>
+                <img src={logo} alt="Cybrix" />
+            </div>
+
+            <div className='form'>
+                <input
+                    type="text"
+                    value={url}
+                    onChange={handleInputChange}
+                    placeholder='Enter URL'
+                />
+                <button onClick={handleSubmit}>Submit</button>
+            </div></div>
+        </div>
+    )
+}
+
+export default Home;
